d579294e3277bd9bcbe0483188d91f0875886c26
Nestyles <tom.sancho@epitech.eu>
2025-06-11 15:11:41 +0800

    [ADD] API routes to register and log a user in a SQLite database

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,14 @@
+from datetime import timedelta
 from typing import Union
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from jose import JWTError, jwt
 
-from fastapi import FastAPI
 from routers.cost_estimation import router as cost_estimation_router
+from database import models
+from database.models import SessionLocal
+import schemas, security
 
 app = FastAPI()
 app.include_router(cost_estimation_router, prefix="/cost-estimation", tags=["Cost Estimation"])
@@ -9,8 +16,3 @@ app.include_router(cost_estimation_router, prefix="/cost-estimation", tags=["Cos
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
